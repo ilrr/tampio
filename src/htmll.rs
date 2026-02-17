2e70353b4d4577272808cabdedaf6f19dc212f99
ilrr <ilari.rauhala@gmail.com>
2026-02-17 14:56:00 +0200

    charset meta tag
    <meta charset="utf-8">

--- src/htmll.rs
+++ src/htmll.rs
@@ -743,7 +743,8 @@ impl Ledger {
         head.push_child(
             Html::new("script").with_raw(include_str!("../html_assets/mini/script.js")),
         );
-        head.push_raw(r#"<link rel="preconnect" href="https://fonts.googleapis.com">
+        head.push_raw(r#"<meta charset="utf-8">
+<link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">"#);
